b349c3430d9f0d6e36483407e048d4af162bf8ac
Captain-Kanak <kanakroy835@gmail.com>
2026-03-27 15:56:58 +0600

    feat: next theme toggle added

---
+++ src/components/layouts/Logo.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Logo() {
+  const logo = {
+    url: "/",
+    src: "/logo.png",
+    alt: "logo",
+    title: "ECO-SPARK-HUB",
+  };
+
+  return (
+    <Link
+      href={logo.url}
+      className="flex items-center gap-2 group transition-opacity hover:opacity-90"
+    >
+      <Image
+        src={logo.src}
+        width={32}
+        height={40}
+        alt={logo.alt}
+        className="dark:invert h-auto w-auto"
+      />
+      <span className="text-lg font-black tracking-tighter bg-linear-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
+        {logo.title}
+      </span>
+    </Link>
+  );
+}
